f1831f75ac49f3ad27de3a7e11d920884956628c
Anderson Sipriano <Sipriano14x@gmail.com>
2021-12-27 22:21:54 -0300

    Mapeando a Entidade Cliente para JPA

--- src/main/java/com/study/jpa/domain/entity/Cliente.java
+++ src/main/java/com/study/jpa/domain/entity/Cliente.java
@@ -1,8 +1,18 @@
 package com.study.jpa.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table( name = "cliente" )//Aqui é necessário o @Table quando o nome da classe é diferente do nome da entidade
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)//h2 db utiliza o AUTO
+    @Column( name = "id" )//não precisaria pois os nomes são iguais da classe e da entidade
     private Integer id;
+    @Column( name = "nome", length = 100) //mesmo tanto que varchar la no banco, denovo não precisava pois
+    // ao colocar @Entity o JPA ja sabe que são colunas, mas precisa se os nomes dos atributos e das entidadades
+    // nos bancos de dados forem diferentes
     private String nome;
 
     public Cliente() {
